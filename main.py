2304680147fdc071afd03857d004165ae1feb61a
Deepak Shivani <64629430+pakodaengineer@users.noreply.github.com>
2025-06-03 09:33:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from sql_solution import extract_with_sql
+import os
+from config import DB_PATH
+from db_create import create_db
 from pandas_solution import extract_with_pandas
 
 def main():
+    # checking if the db exists
+    if not os.path.exists(DB_PATH):
+        print("creating DB")
+        create_db()
+    
+    # checking if the output dir exists
+    if not os.path.exists('output'):
+        os.makedirs('output')
+        print("Created output directory")
+    
+
     print("Running SQL solution.")
     extract_with_sql()
     print("SQL solution completed and CSV generated.\n")
